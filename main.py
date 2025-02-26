94ac3ea82d889aed24bd630afc526b2664df8e48
cbrookscode <colbybrooks@myyahoo.com>
2025-02-26 00:23:51 -0500

    added way to many functions in one day. CLI loop created, options menu displayed, user input driven, creates dictionary into json text file if it doesn't exist. prints out price sheet, update essence functionality works, update price info from poeninja works, exit functionality works. need to seperate out functions logically in webscrape.py file, create unit tests to ensure no bugs, fix any wholes in logic, and add last remaining option in menu (3-1 candidates).

--- main.py
+++ main.py
@@ -1,63 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-#  locate elements on a page (e.g., by tag name, class name, etc.).
-from selenium.webdriver.common.by import By
-# allows waiting until an element appears instead of using time.sleep()
-from selenium.webdriver.support.ui import WebDriverWait
-# provides pre-built conditions (like waiting for an element to be visible)
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-import time
-import re
+from webscrape import *
 
-# Need to pip install selenium, beautifulsoup4, lxml
-# need to wget install linux based chrome (https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb) and webdriver from https://googlechromelabs.github.io/chrome-for-testing/#stable. location of webdriver needs to be put in /usr/local/bin/
+# need check to see if dictionary already created or not. should only happen once.
+create_essence_dictionary()
 
-
-
-
-
-
-options = Options()
-options.add_argument('--headless')  # Runs Chrome in headless mode (no UI)
-
-chromedriver_path = "/usr/local/bin/chromedriver"
-service = Service(chromedriver_path)
-driver = webdriver.Chrome(service=service, options=options)
-driver.get('https://poe.ninja/economy/phrecia/essences')
-
-#dynamically wait (max of 30 seconds) till body tagged html elements load.
-WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.TAG_NAME, "body")))
-html_content = driver.page_source
-driver.quit()
-soup = BeautifulSoup(html_content, 'lxml')
-rows = soup.find_all('tr')[1:]
-
-
-# # Parse table data in html content and create dictionary with key equaling the essence and value equaling its chaos value
-ess_dict = {}
-for row in rows:
-    row_text = row.get_text(strip=True)
-    row_text = row_text.replace("wiki", "")
-    modified_row = re.split(r'(\d+)', row_text, maxsplit=1)
-
-    essence_name = modified_row[0]
-
-    if len(modified_row) != 3:
+i = 1
+while i == 1:
+    program_loop()
+    user_input = input("Which option would you like to select? ")
+    check = implement_user_options(user_input)
+    if check == 0:
         break
-    else:
-        delim = "Not enough data"
-        if delim in "".join(modified_row[1:]):
-            chaos_value = re.split(f"{delim}", "".join(modified_row[1:]), maxsplit=1)
-            ess_dict[essence_name] = chaos_value[0][1:]
 
-        elif "+" in "".join(modified_row[1:]):
-            chaos_value = re.split(r"\+", "".join(modified_row[1:]), maxsplit=1)
-            ess_dict[essence_name] = chaos_value[0][1:]
 
-        else:
-            chaos_value = re.split(r"-", "".join(modified_row[1:]), maxsplit=1)
-            ess_dict[essence_name] = chaos_value[0][1:]
-
-print(ess_dict)
+# create logical seperation of functions into different files.
+# create unit tests
+# Fix holes in existing logic
+# Add last remaining function in options list
